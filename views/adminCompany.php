d924e378d1a796d4ca0e157f39b0c4367e553368
Juan Sebastian Ossa Dominquez <juansebastianossadominguez@gmail.com>
2020-05-24 18:10:07 -0500

    add landing page

--- views/adminCompany.php
+++ views/adminCompany.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>tbl_empresas</title>
+        <title>Gestion Empresas</title>
         <meta charset="UTF-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
